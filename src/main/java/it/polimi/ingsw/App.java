3a58a3b421ae708c5e4413fd1a5b30b82a406d05
Francesco Stucchi <francesco2.stucchi@mail.polimi.it>
2020-03-27 17:22:22 +0100

    Modified classes.

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw;
 
 import it.polimi.ingsw.controller.Controller;
 import it.polimi.ingsw.model.Model;
+import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.view.View;
 
 /**
@@ -11,9 +12,9 @@ import it.polimi.ingsw.view.View;
 public class App 
 {
     public static void main(String [] args){
-        Model model = new Model();
+        Model model = new Model(new Player[2]);
         View view = new View();
-        Controller controller = new Controller(model, view);
+        Controller controller = new Controller(model);
         view.addObserver(controller);
         model.addObserver(view);
         view.run();
