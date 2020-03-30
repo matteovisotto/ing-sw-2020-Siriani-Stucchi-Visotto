b0db0df3622cdda269e58707abc97060974da665
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-03-30 16:19:30 +0200

    Add first test

---
+++ src/test/java/it/polimi/ingsw/model/CellTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CellTest {
+
+    @Test
+    public void getX() {
+        Cell cell = new Cell(1,2);
+        assertTrue("Result", 1==cell.getX());
+    }
+
+    @Test
+    public void getY() {
+    }
+}
