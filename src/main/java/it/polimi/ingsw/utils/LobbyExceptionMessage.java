a79a322418e8bbfd51858aab9a763e30cf8300e1
Francesco Stucchi <francesco2.stucchi@mail.polimi.it>
2020-03-30 15:32:50 +0200

    grammar fixed

--- src/main/java/it/polimi/ingsw/utils/LobbyExceptionMessage.java
+++ src/main/java/it/polimi/ingsw/utils/LobbyExceptionMessage.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.utils;
 
 public class LobbyExceptionMessage {
     public static final String NO_LOBBY = "No lobby available. Please create a new one.";
-    public static final String FULL_LOBBY = "This lobby is full. Please choose an other one or create a new one.";
+    public static final String FULL_LOBBY = "This lobby is full. Please choose another one or create a new one.";
     public static final String INVALID_LOBBY = "You ask for an invalid lobby. Insert a number in the list.";
     public static final String UNAVAILABLE_NAME = "Sorry, this username is already in use in this lobby. Please join with a different username.";
 }
