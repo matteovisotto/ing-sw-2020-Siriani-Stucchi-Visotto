9beee675551fbc9a45dc33c438e0e10541689230
TiaSirio <61708227+TiaSirio@users.noreply.github.com>
2020-04-11 16:28:34 +0200

    Added Phase test.

---
+++ src/test/java/it/polimi/ingsw/model/PhaseTest.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class PhaseTest {
+
+    public class BlocksTest {
+        @Rule
+        public final ExpectedException exception = ExpectedException.none();
+        @Test
+        public void getPhaseId() {
+            assertEquals(-3, Phase.DRAWCARD.getPhaseId());
+            assertEquals(-2, Phase.SETWORKER1.getPhaseId());
+            assertEquals(-1, Phase.SETWORKER2.getPhaseId());
+            assertEquals(0, Phase.WAIT.getPhaseId());
+            assertEquals(1, Phase.BEGINNING.getPhaseId());
+            assertEquals(2, Phase.MOVE.getPhaseId());
+            assertEquals(3, Phase.BUILD.getPhaseId());
+
+        }
+
+        @Test
+        public void getPhase() {
+            assertEquals(Phase.getPhase(-3), Phase.DRAWCARD);
+            assertEquals(Phase.getPhase(-2), Phase.SETWORKER1);
+            assertEquals(Phase.getPhase(-1), Phase.SETWORKER2);
+            assertEquals(Phase.getPhase(0), Phase.WAIT);
+            assertEquals(Phase.getPhase(1), Phase.BEGINNING);
+            assertEquals(Phase.getPhase(2), Phase.MOVE);
+            assertEquals(Phase.getPhase(3), Phase.BUILD);
+        }
+
+        @Test
+        public void getPhaseException(){
+            exception.expect(IllegalArgumentException.class);
+            Phase.getPhase(10);
+        }
+    }
+}
