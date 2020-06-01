a9b9e86dd5859f3dac3154b4fe165170106a0eb6
Francesco Stucchi <francesco2.stucchi@mail.polimi.it>
2020-06-01 17:56:35 +0200

    removed the cli option from the start menu (you now have to use the cli and run with the argument -cli)

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -11,7 +11,7 @@ public class ClientApp
     public static void main(String[] args){
         char graphicInterface = 'a';
         Scanner in = new Scanner(System.in);
-        do{
+        /*do{
             try{
                 System.out.println("Would you like to use the graphic interface?(y/n)");
                 graphicInterface = in.nextLine().toLowerCase().charAt(0);
@@ -19,11 +19,16 @@ public class ClientApp
 
             }
         }while(graphicInterface != 'y' && graphicInterface != 'n');
-
+*/
         try{
-            if(graphicInterface == 'n'){
-                Client client = new Client("127.0.0.1", 12345);
-                client.run();
+            if(args.length!=0){
+                if(args[0].equals("-cli")){
+                    Client client = new Client("127.0.0.1", 12345);
+                    client.run();
+                }
+                else{
+                    System.out.println("Wrong args");
+                }
             }
             else{
                 GUIClient client = new GUIClient("127.0.0.1", 12345);
