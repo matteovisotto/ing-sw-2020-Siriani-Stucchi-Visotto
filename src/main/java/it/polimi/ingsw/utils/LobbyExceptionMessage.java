ba34173fe20421a4224da9f641f4de2ff8af090a
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-06-03 19:29:49 +0200

    Fix error messagges

--- src/main/java/it/polimi/ingsw/utils/LobbyExceptionMessage.java
+++ src/main/java/it/polimi/ingsw/utils/LobbyExceptionMessage.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw.utils;
 
 public class LobbyExceptionMessage {
-    public static final String NO_LOBBY = "No lobby available. Please create a new one.";
-    public static final String FULL_LOBBY = "This lobby is full. Please choose another one or create a new one.";
-    public static final String INVALID_LOBBY = "You ask for an invalid lobby. Insert a number in the list.";
-    public static final String UNAVAILABLE_NAME = "Sorry, this username is already in use in this lobby. Please join with a different username.";
+    public static final String NO_LOBBY = "ERROR: No lobby available. Please create a new one.";
+    public static final String FULL_LOBBY = "ERROR: This lobby is full. Please choose another one or create a new one.";
+    public static final String INVALID_LOBBY = "ERROR: You ask for an invalid lobby. Insert a number in the list.";
+    public static final String UNAVAILABLE_NAME = "ERROR: Sorry, this username is already in use in this lobby. Please join with a different username.";
 }
