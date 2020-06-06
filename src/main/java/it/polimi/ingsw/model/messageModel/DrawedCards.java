170e8e519b5d0d1d1dd718c54354b0ead75bbdd3
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-06-06 23:29:05 +0200

    Completed MessageModel JavaDoc

--- src/main/java/it/polimi/ingsw/model/messageModel/DrawedCards.java
+++ src/main/java/it/polimi/ingsw/model/messageModel/DrawedCards.java
@@ -5,16 +5,34 @@ import it.polimi.ingsw.controller.GodCardController;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.view.View;
 
+/**
+ * This class is used to notify the controller of the selected cards for the play by the first player
+ */
 public class DrawedCards extends Message {
     int firstCard;
     int secondCard;
     int thirdCard;
+
+    /**
+     * Class constructor for a two players play
+     * {@inheritDoc}
+     * @param firstCard first card selected
+     * @param secondCard second card selected
+     */
     public DrawedCards(Player player, int firstCard, int secondCard,View view) {
         super(player, view);
         this.firstCard = firstCard;
         this.secondCard = secondCard;
         this.thirdCard = -1;
     }
+
+    /**
+     * Class constructor for a three players play
+     * {@inheritDoc}
+     * @param firstCard first card selected
+     * @param secondCard second card selected
+     * @param  thirdCard third card selected
+     */
     public DrawedCards(Player player, int firstCard, int secondCard, int thirdCard, View view) {
         super(player, view);
         this.firstCard = firstCard;
@@ -22,17 +40,34 @@ public class DrawedCards extends Message {
         this.thirdCard = thirdCard;
     }
 
+    /**
+     *
+     * @return the first selected card id
+     */
     public int getFirst(){
         return firstCard;
     }
+
+    /**
+     *
+     * @return the second selected card id
+     */
     public int getSecond(){
         return secondCard;
     }
+
+    /**
+     *
+     * @return the third selected card id
+     */
     public int getThird(){
         return thirdCard;
     }
 
-
+    /**
+     * Call the controller drawedCards function for this configuration
+     * @param controller thr game controller instance
+     */
     @Override
     public void handler(Controller controller) {
         ((GodCardController)controller).drawedCards(this);
--- src/main/java/it/polimi/ingsw/model/messageModel/PickedCard.java
+++ src/main/java/it/polimi/ingsw/model/messageModel/PickedCard.java
@@ -5,18 +5,35 @@ import it.polimi.ingsw.controller.GodCardController;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.view.View;
 
+/**
+ * This class is uset to notify the controller of a card selected by the player
+ */
 public class PickedCard extends Message {
     private final int card;
+
+    /**
+     * Class constructor
+     * {@inheritDoc}
+     * @param card an int value that represent the card selected by the player.
+     */
     public PickedCard(Player player, View view, int card) {
         super(player, view);
         this.card = card;
     }
 
+    /**
+     * Call controller pickACard function for this configuration
+     * @param controller thr game controller instance
+     */
     @Override
     public void handler(Controller controller) {
         ((GodCardController)controller).pickACard(this);
     }
 
+    /**
+     *
+     * @return the id of the selected card
+     */
     public int getCardId() {
         return card;
     }
