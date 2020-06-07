b6fd524661195bafc6c478b3b65cb7e0f7600d3b
Francesco Stucchi <francesco2.stucchi@mail.polimi.it>
2020-06-07 22:20:12 +0200

    Poseidon fixed (need to fix the GUI)

--- src/main/java/it/polimi/ingsw/model/simplegod/Poseidon.java
+++ src/main/java/it/polimi/ingsw/model/simplegod/Poseidon.java
@@ -8,39 +8,49 @@ import java.util.List;
 
 public class Poseidon  extends GodCard {
     private int counter=0;
-    private boolean usedPower;
-    private Worker usedWorker;
+    private Worker unusedWorker=null;
+    private Worker movedWorker=null;
     /**
      * {@inheritDoc}
      */
     public Poseidon() {
         super(Gods.POSEIDON, Phase.BUILD);
         counter=0;
-        usedPower=false;
-    }
-    public boolean hasUsedPower(){
-        return this.usedPower;
     }
 
     public int getNumOfBuild(){
         return this.counter;
     }
 
-    public void setUsedWorker(Worker worker){
-        this.usedWorker=worker;
+    public void resetBuild(){
+        counter=0;
     }
 
+    public void setUnusedWorker(Worker worker){
+        this.unusedWorker =worker;
+    }
+
+    public Worker getUnusedWorker(){
+        return this.unusedWorker;
+    }
     /**
      * {@inheritDoc}
      */
     @Override
     public void usePower(List<Object> objectList) {
-        this.usedPower=true;
         counter++;
         Model model = (Model)objectList.get(0);
         model.setNextPhase(getPhase());
         model.setNextPlayerMessage(PlayerMessage.BUILD);
         model.setNextMessageType(MessageType.BUILD);
         model.notifyChanges();
+    }
+
+    public Worker getMovedWorker() {
+        return movedWorker;
+    }
+
+    public void setMovedWorker(Worker movedWorker) {
+        this.movedWorker = movedWorker;
     }
 }
