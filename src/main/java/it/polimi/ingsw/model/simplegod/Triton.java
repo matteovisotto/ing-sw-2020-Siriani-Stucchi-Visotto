5df5cfed790266b92ace698b7e2728a749a8348f
Francesco Stucchi <francesco2.stucchi@mail.polimi.it>
2020-06-07 23:33:13 +0200

    Trtiton implemented

--- src/main/java/it/polimi/ingsw/model/simplegod/Triton.java
+++ src/main/java/it/polimi/ingsw/model/simplegod/Triton.java
@@ -2,23 +2,42 @@ package it.polimi.ingsw.model.simplegod;
 
 import it.polimi.ingsw.model.GodCard;
 import it.polimi.ingsw.model.Gods;
+import it.polimi.ingsw.model.Model;
 import it.polimi.ingsw.model.Phase;
+import it.polimi.ingsw.model.messageModel.MessageType;
+import it.polimi.ingsw.utils.PlayerMessage;
 
 import java.util.List;
 
 public class Triton extends GodCard {
+    private int usedWorkerID =-1;
+    private boolean usedPower;
     /**
      * {@inheritDoc}
      */
     public Triton() {
         super(Gods.TRITON, Phase.MOVE);
+        usedWorkerID =-1;
     }
+
+    public int getUsedWorkerID(){
+        return this.usedWorkerID;
+    }
+    public void setUsedWorkerID(int worker){
+        this.usedWorkerID =worker;
+    }
+
+
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void usePower(List<Object> objectList) {
-
+        Model model = (Model)objectList.get(0);
+        model.setNextPhase(getPhase());
+        model.setNextPlayerMessage(PlayerMessage.MOVE);
+        model.setNextMessageType(MessageType.MOVE);
+        model.notifyChanges();
     }
 }
