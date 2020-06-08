745f7af15f5f4146b744d46643baecf47d3b062f
TiaSirio <61708227+TiaSirio@users.noreply.github.com>
2020-06-09 00:30:42 +0200

    Add Zeus test.

--- src/test/java/it/polimi/ingsw/controller/GodCardControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/GodCardControllerTest.java
@@ -1437,4 +1437,66 @@ public class GodCardControllerTest {
 
         assertEquals(model.getPhase(), Phase.MOVE);
     }
+
+    @Test
+    public void useZeusPowerTest() {
+        Player[] players = new Player[2];
+        players[0] = new Player("Mario");
+        players[1] = new Player("Luigi");
+        Model model = new Model(players, false);
+        GodCardController controller = new GodCardController(model);
+        RemoteView remoteView = new RemoteView(players[0], players[1].getPlayerName(), new ClientConnection() {
+            @Override
+            public void closeConnection() {
+
+            }
+
+            @Override
+            public void send(Object message) {
+
+            }
+
+            @Override
+            public void asyncSend(Object message) {
+
+            }
+        },null);
+        DrawedCards drawedCards = new DrawedCards(players[0], 0, 13, remoteView);
+        controller.drawedCards(drawedCards);
+        RemoteView remoteView2 = new RemoteView(players[1], players[0].getPlayerName(), new ClientConnection() {
+            @Override
+            public void closeConnection() {
+
+            }
+
+            @Override
+            public void send(Object message) {
+
+            }
+
+            @Override
+            public void asyncSend(Object message) {
+
+            }
+        },null);
+        PickedCard pickedCard = new PickedCard(players[1], remoteView2, 0);
+        controller.pickACard(pickedCard);
+
+        PlayerWorker playerWorker = new PlayerWorker(players[0], 0, 0, remoteView);
+        controller.setPlayerWorker(playerWorker);
+        PlayerWorker playerWorker2 = new PlayerWorker(players[0], 1, 1, remoteView);
+        controller.setPlayerWorker(playerWorker2);
+
+        PlayerWorker playerWorker3 = new PlayerWorker(players[1], 2, 0, remoteView2);
+        controller.setPlayerWorker(playerWorker3);
+        PlayerWorker playerWorker4 = new PlayerWorker(players[1], 3, 2, remoteView2);
+        controller.setPlayerWorker(playerWorker4);
+
+        PlayerMove playerMove = new PlayerMove(players[0], 0, 0, 1, remoteView);
+        controller.move(playerMove);
+
+        PlayerBuild playerBuild = new PlayerBuild(players[0], players[0].getUsedWorker(), 0, 1, remoteView);
+        controller.build(playerBuild);
+        assertEquals(model.getBoard().getCell(0,1).getLevel().getBlockId(),1);
+    }
 }
