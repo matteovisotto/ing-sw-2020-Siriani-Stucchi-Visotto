8d3c5c415548c26981c9338baad14717e9baafed
TiaSirio <61708227+TiaSirio@users.noreply.github.com>
2020-06-30 13:02:17 +0200

    Add print test.

--- src/test/java/it/polimi/ingsw/model/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.model.messageModel.PlayerWorker;
+import it.polimi.ingsw.server.ClientConnection;
+import it.polimi.ingsw.view.RemoteView;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -42,7 +46,42 @@ public class BoardTest {
         Player[] players = new Player[2];
         players[0] = new Player("pioppo");
         players[1] = new Player("Amedeus");
-        Board board = new Board(players);
+        Model model = new Model(players,true);
+        Controller controller = new Controller(model);
+        RemoteView remoteView = new RemoteView(players[0], players[1].getPlayerName(), new ClientConnection() {
+            @Override
+            public void closeConnection() {
+
+            }
+
+            @Override
+            public void send(Object message) {
+
+            }
+
+            @Override
+            public void asyncSend(Object message) {
+
+            }
+        }, null);
+
+        RemoteView remoteView1 = new RemoteView(players[1], players[0].getPlayerName(), new ClientConnection() {
+            @Override
+            public void closeConnection() {
+
+            }
+
+            @Override
+            public void send(Object message) {
+
+            }
+
+            @Override
+            public void asyncSend(Object message) {
+
+            }
+        },null);
+
         String string = "         0         1         2         3         4\n" +
                 "    ---------------------------------------------------\n" +
                 "0   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
@@ -60,7 +99,36 @@ public class BoardTest {
                 "4   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
                 "    | P:n W:n | P:n W:n | P:n W:n | P:n W:n | P:n W:n |\n" +
                 "    ---------------------------------------------------\n";
-        //string;
-        assertEquals(string,board.print());
+        assertEquals(string,model.getBoard().print());
+
+        PlayerWorker playerWorker = new PlayerWorker(players[0],0,0,remoteView);
+        controller.setPlayerWorker(playerWorker);
+        PlayerWorker playerWorker2 = new PlayerWorker(players[0],1,1,remoteView);
+        controller.setPlayerWorker(playerWorker2);
+
+        PlayerWorker playerWorker3 = new PlayerWorker(players[1],2,2,remoteView1);
+        controller.setPlayerWorker(playerWorker3);
+        PlayerWorker playerWorker4 = new PlayerWorker(players[1],3,3,remoteView1);
+        controller.setPlayerWorker(playerWorker4);
+
+        String string2 = "         0         1         2         3         4\n" +
+                "    ---------------------------------------------------\n" +
+                "0   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
+                "    | P:0 W:0 | P:n W:n | P:n W:n | P:n W:n | P:n W:n |\n" +
+                "    ---------------------------------------------------\n" +
+                "1   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
+                "    | P:n W:n | P:0 W:1 | P:n W:n | P:n W:n | P:n W:n |\n" +
+                "    ---------------------------------------------------\n" +
+                "2   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
+                "    | P:n W:n | P:n W:n | P:1 W:0 | P:n W:n | P:n W:n |\n" +
+                "    ---------------------------------------------------\n" +
+                "3   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
+                "    | P:n W:n | P:n W:n | P:n W:n | P:1 W:1 | P:n W:n |\n" +
+                "    ---------------------------------------------------\n" +
+                "4   | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | L:0 F:0 | \n" +
+                "    | P:n W:n | P:n W:n | P:n W:n | P:n W:n | P:n W:n |\n" +
+                "    ---------------------------------------------------\n";
+
+        assertEquals(string2,model.getBoard().print());
     }
 }
